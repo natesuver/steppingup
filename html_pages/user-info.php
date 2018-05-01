8069f8e36ea3dd1e5a660b84165d34f04b6b7843
klark17 <32020705+klark17@users.noreply.github.com>
2018-04-30 22:22:11 -0400

    Update user-info.php
    change view of user search results

--- html_pages/user-info.php
+++ html_pages/user-info.php
@@ -24,35 +24,55 @@
         </div>
         <a href="search.php" class="homeLink">Return to Search</a>
 
-        <div class="adminNavBox" id="userSearch">
+        <div class="adminNavBox">
             <?php echo "<h4>User: ".$data['username']."</h4><br>" ?>
 			<table style="width:100%">
+				
 				<tr>
 					<th>First Name</th>
-					<th>Last Name</th> 
-					<th>Address</th>
-					<th>City</th>
-					<th>State</th> 
-					<th>Postal Code</th>
-					<th>Gender</th>
-					<th>Birth Date</th> 
-					<th>Height</th>
-					<th>Weight</th>
-					<th>Occupation</th>
-				 </tr>
-				  <tr>
 					<td><?php echo $data['fName'] ?></td>
+				</tr>
+				<tr>
+					<th>Last Name</th> 
 					<td><?php echo $data['lName'] ?></td>
+				</tr>
+				<tr>
+					<th>Address</th>
 					<td><?php echo $data['address'] ?></td>
+				</tr>
+				<tr>
+					<th>City</th>
 					<td><?php echo $data['city'] ?></td>
+				</tr>
+				<tr>
+					<th>State</th> 
 					<td><?php echo $data['state'] ?></td>
+				</tr>
+				<tr>
+					<th>Postal Code</th>
 					<td><?php echo $data['pCode'] ?></td>
+				</tr>
+				<tr>
+					<th>Gender</th>
 					<td><?php echo $data['gender'] ?></td>
+				</tr>
+				<tr>
+					<th>Birth Date</th> 
 					<td><?php echo $data['birthDate'] ?></td>
+				</tr>
+				<tr>
+					<th>Height</th>
 					<td><?php echo $data['height'] ?></td>
+				</tr>
+				<tr>
+					<th>Weight</th>
 					<td><?php echo $data['weight'] ?></td>
+				</tr>
+				<tr>
+					<th>Occupation</th>
 					<td><?php echo $data['occupation'] ?></td>
-				  </tr>
+				</tr>
+
 			</table>
 		</div>
 		<img id="logo" src="<?php if($_SESSION['useMongo']==0) { echo "https://www.mysql.com/common/logos/logo-mysql-170x115.png";} else {echo "https://zdnet3.cbsistatic.com/hub/i/r/2018/02/16/8abdb3e1-47bc-446e-9871-c4e11a46f680/resize/370xauto/8a68280fd20eebfa7789cdaa6fb5eff1/mongo-db-logo.png";} ?>" class="db-logo"></img>
